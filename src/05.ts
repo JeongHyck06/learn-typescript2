177cacaba851d7050dfb8f975021a54f74d2d5c2
Jack <devjack050700@gmail.com>
2024-11-12 11:36:59 +0900

    함수

---
+++ src/05.ts
@@ -0,0 +1,11 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+const result1 = add(1, 2);
+
+const minus = (a: number, b: number) => {
+    return a - b;
+};
+const result2 = minus(5, 3);
+
+let temp: Function;

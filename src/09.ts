ac34a2bf98f5331539afc5caa9624cc78df0e73f
Jack <devjack050700@gmail.com>
2024-11-12 15:53:55 +0900

    접근제한자

---
+++ src/09.ts
@@ -0,0 +1,36 @@
+// class Book {
+//     /**
+//      * @public
+//      * @protected
+//      * @private
+//      */
+//     private readonly title: string;
+//     private readonly a: string;
+//     private readonly b: string;
+
+//     constructor(title: string, a: any, b: any) {
+//         this.title = title;
+//         this.a = a;
+//         this.b = b;
+//     }
+// }
+
+class Book {
+    constructor(private readonly title: string, private readonly a: string, private readonly b: string) {
+        this.title = title;
+        this.a = a;
+        this.b = b;
+    }
+}
+
+const b: Book = new Book('inflearn', 'hello', 'world');
+console.log(b);
+
+// class Test extends Book {
+//     constructor(title: string) {
+//         super(title);
+//     }
+//     printTitle() {
+//         console.log(this.title);
+//     }
+// }

9b811dd7e1fcf44bfd4ddd569026f983461d97b1
Jack <devjack050700@gmail.com>
2024-11-22 09:41:33 +0900

    type 인터페이스 vs interface 인터페이스 (보충)

--- src/section02/01.ts
+++ src/section02/01.ts
@@ -1,47 +1,50 @@
-// type TUser = {
-//     id: number;
-//     name: string;
-// };
+type TUser = {
+    id: number;
+    name: string;
+};
 
-import { create } from 'ts-node';
+type TPost = {
+    name: string;
+    content: string;
+};
 
-// type TPost = {
-//     name: string;
-//     content: string;
-// };
+type TUserWithPost = TUser & TPost;
 
-// type TUserWithPost = TUser & TPost;
+type TUserWithPostWithCreatedAt = TUserWithPost & {
+    createdAt: Date;
+};
 
-// type TUserWithPostWithCreatedAt = TUserWithPost & {
-//     createdAt: Date;
-// };
+type TUserWithPostWithUpdatedAt = TUserWithPost & {
+    upadateAt: Date;
+};
 
-// type TUserWithPostWithUpdatedAt = TUserWithPost & {
-//     upadateAt: Date;
-// };
+const temp2: TUserWithPost & {
+    updateAt: Date;
+} = {
+    id: 1,
+    name: 'inflearn',
+    content: 'typescript',
+    updateAt: new Date(),
+};
 
-// const temp2: TUserWithPost & {
-//     updateAt: Date;
-// } = { id: 1, name: 'inflearn', content: 'typescript', updateAt: new Date() };
+interface IUser {
+    id: number;
+    name: string;
+}
 
-// interface IUser {
-//     id: number;
-//     name: string;
-// }
+interface IPost {
+    name: string;
+    content: string;
+}
 
-// interface IPost {
-//     name: string;
-//     content: string;
-// }
+interface IUserWithPost extends IUser, IPost {}
+interface IUserWithPostWithCreatedAt extends IUserWithPost {
+    createdAt: Date;
+}
 
-// interface IUserWithPost extends IUser, IPost {}
-// interface IUserWithPostWithCreatedAt extends IUserWithPost {
-//     createdAt: Date;
-// }
-
-// interface IUserWithPostWitUpdateAt extends IUserWithPost {
-//     updateAt: Date;
-// }
+interface IUserWithPostWitUpdateAt extends IUserWithPost {
+    updateAt: Date;
+}
 
 const something2 = {
     id: 1,
@@ -54,4 +57,28 @@ const something2 = {
     },
 } as const;
 
+// 객체
 type ISomtiong = typeof something2;
+
+type UserType = {
+    name: number;
+    height: number;
+    marketing: boolean;
+};
+
+//클래스
+interface IUser2 {
+    name: number;
+    height: number;
+    marketing: boolean;
+}
+
+type SomeType = {
+    [props in keyof UserType]: string;
+};
+
+const mock: SomeType = {
+    name: 'inflaern',
+    height: '176',
+    marketing: 'true',
+};
